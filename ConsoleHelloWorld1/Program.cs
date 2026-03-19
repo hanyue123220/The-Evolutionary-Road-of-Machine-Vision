b29ee6119e8d975ce4acd36aba30901ec80e26de
hanyue123220 <2380793184@qq.com>
2026-03-19 16:47:43 +0800

    第一个控制台输出helloworld项目

---
+++ ConsoleHelloWorld1/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleHelloWorld1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("nihao");
+            Console.WriteLine("nihao1");
+        }
+    }
+}
